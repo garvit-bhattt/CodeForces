efc1f4f04280ae5d3e37238e748d29475ec29ab2
GARVIT <garvitbhattpc@gmail.com>
2026-01-21 21:57:13 +0530

    easy prob added

---
+++ CodeForces/easy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for (int n=0;n<t;n++){
+        int ans;
+        cin>>ans;
+        cout<<(ans-1)<<"\n";
+    }
+    return 0;
+}
