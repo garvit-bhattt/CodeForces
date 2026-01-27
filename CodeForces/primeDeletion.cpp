3706d92108f488920144af89f40dcee3af53e6ba
GARVIT <garvitbhattpc@gmail.com>
2026-01-27 12:21:29 +0530

    prime deletion problem solved

---
+++ CodeForces/primeDeletion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void solve(){
+    string s;
+    cin>>s;
+    for (int i=0;i<s.size();i++){
+        if (s[i]=='1'){
+            cout<<"13"<<"\n";
+            break;
+        }
+        else if(s[i]=='3'){
+            cout<<"31"<<"\n";
+            break;
+        }
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
