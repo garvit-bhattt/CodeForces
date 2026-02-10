3d4a804dc5255f89cc8cd5bbddf20bb496aa276e
GARVIT <garvitbhattpc@gmail.com>
2026-02-10 20:52:55 +0530

    helpful maths prob solved

---
+++ CodeForces/Helpfulmaths.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    vector<char> nums;
+    for (char c : s) {
+        if (c != '+') nums.push_back(c);
+    }
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i];
+        if (i + 1 < nums.size()) cout << "+";
+    }
+
+    return 0;
+}
